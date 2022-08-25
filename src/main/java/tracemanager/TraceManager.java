e247e98da8adc75194a45561329468648a9f8dd3
bchenghi <bohchenghin@gmail.com>
2022-08-26 02:10:44 +0800

    Update with test io

--- src/main/java/tracemanager/TraceManager.java
+++ src/main/java/tracemanager/TraceManager.java
@@ -1,14 +1,20 @@
 package tracemanager;
 
 import jmutation.model.TestCase;
-import jmutation.model.TestIO;
+import testio.TestIOFramework;
+import testio.model.TestIO;
+import jmutation.mutation.commands.MutationCommand;
 import microbat.model.trace.TraceNode;
 import tracediff.TraceDiff;
 import jmutation.MutationFramework;
 import jmutation.model.MutationResult;
 import jmutation.model.Project;
 import tracediff.model.PairList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TraceManager {
     MutationFramework mutationFramework = new MutationFramework();
@@ -25,18 +31,36 @@ public class TraceManager {
     public void run() {
         mutationFramework.setProjectPath("../java-mutation-framework/sample/math_70");
         mutationFramework.setDropInsDir("../java-mutation-framework/lib");
+        mutationFramework.setMicrobatConfigPath("../java-mutation-framework/sampleMicrobatConfig.json");
         List<TestCase> testCases = mutationFramework.getTestCases();
-        for (TestCase test: testCases) {
+        for (int i = 0; i < testCases.size(); i++) {
+            int seed = 1;
+            int numOfMutations = 1;
+            TestCase test = testCases.get(i);
+            mutationFramework.setSeed(seed);
             mutationFramework.setTestCase(test);
-            MutationResult mutationResult = mutationFramework.startMutationFramework();
+            mutationFramework.setMaxNumberOfMutations(numOfMutations);
+            MutationResult mutationResult;
+            mutationResult = mutationFramework.startMutationFramework();
+
             List<TraceNode> rootCauses = mutationResult.getRootCauses();
-            List<TestIO> testIOs = mutationResult.getTestIOs();
+
+            List<MutationCommand> mutationHistory = mutationResult.getMutationHistory();
+
             Project mutatedProject = mutationResult.getMutatedProject();
             Project originalProject = mutationResult.getOriginalProject();
-            PairList pairList = traceDiff.getTraceAlignment("src\\main\\java", "src\\main\\test",
+
+            PairList pairList = traceDiff.getTraceAlignment("src\\main\\java", "src\\test\\java",
                     mutatedProject.getRoot().getAbsolutePath(), originalProject.getRoot().getAbsolutePath(),
                     mutationResult.getMutatedTrace(), mutationResult.getOriginalTrace());
-            System.out.println("Pair list obtained");
+
+            TestIOFramework testIOFramework = new TestIOFramework();
+            TestIO testIO = testIOFramework.getBuggyTestIOs(mutationResult.getOriginalResult(),
+                    mutationResult.getOriginalResultWithAssertions(),
+                    mutationResult.getMutatedResult(),
+                    mutationResult.getMutatedResultWithAssertions(), originalProject.getRoot(),
+                    mutatedProject.getRoot(), pairList, mutationResult.getTestClass(),
+                    mutationResult.getTestSimpleName());
         }
     }
 }
